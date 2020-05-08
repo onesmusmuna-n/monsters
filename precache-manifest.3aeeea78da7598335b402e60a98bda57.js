1ecd2e343a2b98feaf58cb6678a6794706223ccb
Onesmux <43335776+Onesmux@users.noreply.github.com>
2020-05-08 13:24:30 +0300

    Updates

---
+++ precache-manifest.3aeeea78da7598335b402e60a98bda57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a86bbf8877d07070c981650f935a65",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "9a7d058f5d1903e23ea5",
+    "url": "/monsters/static/css/main.df5dc2d9.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7d058f5d1903e23ea5",
+    "url": "/monsters/static/js/main.7c8e7b15.chunk.js"
+  },
+  {
+    "revision": "28c7cc27b21cfacfd62e",
+    "url": "/monsters/static/js/runtime-main.8d89be33.js"
+  }
+]);
